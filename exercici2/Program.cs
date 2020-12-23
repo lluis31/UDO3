1741792d370dd49a8d91cd201b83b3bc02dede5d
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-23 17:19:54 +0100

    exercici2

---
+++ exercici2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercici2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Hello World!);
+            int N = 5;
+            double A = 4.56;
+            char C = 'a';
+
+            Console.WriteLine("N={0}, A={1}, C={2}", N, A, C);
+            Console.WriteLine("La suma de N+A {0}", N + A);
+            Console.WriteLine("La diferencia de A-N {0}", A - N);
+            Console.WriteLine("El valor numerico del caracter C {0}", (int)C);
+        }
+    }
+}
