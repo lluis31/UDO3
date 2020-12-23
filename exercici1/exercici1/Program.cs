d77f579096e291dbb28937cfa8c6e13ede791ca8
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-23 14:24:12 +0100

    udo3

---
+++ exercici1/exercici1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercici1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            Console.WriteLine("Suma: {0}, Resta: {1}, Mult:{2}, Div:{3}, Modulo: {4}", x + y, x - y, x * y, x / y, x % y);
+        }
+    }
+}
