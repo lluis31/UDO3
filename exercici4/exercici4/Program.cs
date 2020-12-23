044bd9542eec4594bbcf07a967417731ed3e44b3
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-23 18:39:45 +0100

    exercici4

---
+++ exercici4/exercici4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercici4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 20;
+            Console.WriteLine("Incrementar N={0}", N + 77);
+            Console.WriteLine("Decrementar N={0}", N - 3);
+            Console.WriteLine("Doble N={0}", N * 2);
+
+        }
+    }
+}
