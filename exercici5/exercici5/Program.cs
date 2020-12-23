9c49114db04217cdb75f14bc7ee30ad80ea29a56
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-23 20:18:05 +0100

    exercici5

---
+++ exercici5/exercici5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercici5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 12;
+            int C = 8;
+            int D = 19;
+        
+            (B, D) => B == D);
+        }
+    }
+}
